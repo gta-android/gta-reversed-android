d3219e3b2aeaa13cefa4e9596a549fb584910ba0
Roma <38257989+MrJohnDev@users.noreply.github.com>
2025-02-06 00:43:02 +0700

    Remove unused deps (#16)
    * feat(project): remove kotlin, material, other data
    
    * chore(arch): format

--- android/app/src/main/java/com/regta/launcher/MainActivity.java
+++ android/app/src/main/java/com/regta/launcher/MainActivity.java
@@ -1,19 +1,21 @@
 package com.regta.launcher;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+
+import com.regta.core.R;
 import com.regta.core.REGTA;
 
 import java.util.List;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+public class MainActivity extends Activity implements EasyPermissions.PermissionCallbacks {
 
     private static final int MAIN_PERMISSIONS_REQUEST_CODE = 300;
     String[] main_permissions = {
@@ -24,13 +26,15 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.loadingscreen);
         if (EasyPermissions.hasPermissions(this, main_permissions)) {
             startActivity(new Intent(this, REGTA.class));
             this.finish();
         } else {
             EasyPermissions.requestPermissions(this,
                     "The application needs permission to write to memory",
-                    MAIN_PERMISSIONS_REQUEST_CODE, main_permissions);
+                    MAIN_PERMISSIONS_REQUEST_CODE, main_permissions
+            );
         }
     }
 
@@ -38,12 +42,14 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
+
     public void onPermissionsGranted(int requestCode, @NonNull List<String> list) {
-        if(requestCode == MAIN_PERMISSIONS_REQUEST_CODE) {
+        if (requestCode == MAIN_PERMISSIONS_REQUEST_CODE) {
             startActivity(new Intent(this, REGTA.class));
             this.finish();
         }
     }
+
     public void onPermissionsDenied(int requestCode, @NonNull List<String> list) {
         // TODO
     }
